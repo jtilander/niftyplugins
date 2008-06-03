84d9841869dc580197c43b83688077ecbe10ab9e
jim.tilander <jim.tilander@59253a7c-8f1e-0410-beb5-059759380985>
2008-06-03 06:13:50 +0000

    Added small tool command to close all the tool windows.

---
+++ NiftySolution/Commands/CloseToolWindows.cs
@@ -0,0 +1,47 @@
+// Copyright (C) 2006-2007 Jim Tilander. See COPYING for and README for more details.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using EnvDTE;
+using EnvDTE80;
+
+namespace Aurora
+{
+	namespace NiftySolution
+	{
+		class CloseToolWindow : CommandBase
+		{
+			public CloseToolWindow()
+			{
+			}
+
+			public override void OnCommand(DTE2 application, OutputWindowPane pane)
+			{
+				CloseToolWindows(application);
+				
+				// Sometimes this really doesn't "bite", so we need to run through the list again.
+				CloseToolWindows(application);
+			}
+
+			public override bool IsEnabled(DTE2 application)
+			{
+				return true;
+			}
+			
+			private void CloseToolWindows(DTE2 application)
+			{
+				foreach (Window w in application.Windows)
+				{
+					if (!w.Visible)
+						continue;
+
+					if (w.Kind == "Document")
+						continue;
+
+					w.Close(EnvDTE.vsSaveChanges.vsSaveChangesNo);
+				}
+			}
+			
+		}
+	}
+}
