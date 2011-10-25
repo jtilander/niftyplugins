70717b8e2b32465f16513af2835492055ce7755c
jim.tilander <jim.tilander@59253a7c-8f1e-0410-beb5-059759380985>
2011-10-25 20:35:33 +0000

    - Fixed a tricky issue where the process call will actually deadlock if you output more text from a p4 command. Turns out that I need to handle the output in a async fashion to capture it.
    - Didn't have lambdas so the code is a little bit clunky.

--- Shared/Process.cs
+++ Shared/Process.cs
@@ -1,5 +1,6 @@
 // Copyright (C) 2006-2010 Jim Tilander. See COPYING for and README for more details.
 using System;
+using System.Threading;
 
 namespace Aurora
 {
@@ -11,31 +12,74 @@ namespace Aurora
 			public Error(string info_, params object[] vaargs_) { info = string.Format(info_, vaargs_); }
 		};
 
+		// Helper class to capture output correctly and send an event once we've reached the end of the file.
+		private class Handler : IDisposable
+		{
+			public string buffer;
+			public ManualResetEvent sentinel;
+			
+			public Handler()
+			{
+				buffer = "";
+				sentinel = new ManualResetEvent(false);
+			}
+			
+			public void Dispose()
+			{
+				sentinel.Close();
+			}
+			
+			public void OnOutput(object sender, System.Diagnostics.DataReceivedEventArgs e)
+			{
+				if( e.Data == null )
+				{
+					sentinel.Set();
+				}
+				else
+				{
+					buffer = buffer + e.Data + "\n";
+				}
+			}
+		};
+		
 		public static string Execute(string executable, string workingdir, string arguments, params object[] vaargs)
 		{
-			System.Diagnostics.Process process = new System.Diagnostics.Process();
-			process.StartInfo.UseShellExecute = false;
-			process.StartInfo.FileName = executable;
-			process.StartInfo.RedirectStandardOutput = true;
-			process.StartInfo.RedirectStandardError = true;
-			process.StartInfo.CreateNoWindow = true;
-			process.StartInfo.WorkingDirectory = workingdir;
-			process.StartInfo.Arguments = string.Format(arguments, vaargs);
-			
-			if(!process.Start())
+			using( System.Diagnostics.Process process = new System.Diagnostics.Process() )
 			{
-				throw new Error("{0}: Failed to start {1}.", executable, process.StartInfo.Arguments);
-			}
-			process.WaitForExit();
+				process.StartInfo.UseShellExecute = false;
+				process.StartInfo.FileName = executable;
+				process.StartInfo.RedirectStandardOutput = true;
+				process.StartInfo.RedirectStandardError = true;
+				process.StartInfo.CreateNoWindow = true;
+				process.StartInfo.WorkingDirectory = workingdir;
+				process.StartInfo.Arguments = string.Format(arguments, vaargs);
+				
+				if(!process.Start())
+				{
+					throw new Error("{0}: Failed to start {1}.", executable, process.StartInfo.Arguments);
+				}
+				
+				using( Handler stderr = new Handler(), stdout = new Handler() )
+				{
+					process.OutputDataReceived += stdout.OnOutput;
+					process.BeginOutputReadLine();
+					
+					process.ErrorDataReceived += stderr.OnOutput;
+					process.BeginErrorReadLine();
+					
+					process.WaitForExit();
 
-			string stdOut = process.StandardOutput.ReadToEnd();
-			string stdErr = process.StandardError.ReadToEnd();
-
-			if(0 != process.ExitCode)
-			{
-				throw new Error("Failed to execute {0} {1}, exit code was {2}", executable, process.StartInfo.Arguments, process.ExitCode);
+					if(0 != process.ExitCode)
+					{
+						throw new Error("Failed to execute {0} {1}, exit code was {2}", executable, process.StartInfo.Arguments, process.ExitCode);
+					}
+					
+					stderr.sentinel.WaitOne();
+					stdout.sentinel.WaitOne();
+					
+					return stdout.buffer + "\n" + stderr.buffer;
+				}
 			}
-			return stdOut + "\n" + stdErr;
 		}
 	}
 }
